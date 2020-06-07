3617cd6117f0ba15f7861b4329eeabb738dc1e34
Mohammed Yaseen <mohammedysn15@gmail.com>
2020-06-07 19:44:24 +0100

    Split menu methods

--- scripts/main.py
+++ scripts/main.py
@@ -3,33 +3,34 @@ from list_gen import ListGen
 
 class Menu():
     def __init__(self):
-        self.show_menu()
+        self.start_again = True
+        while self.start_again:
+            self.show_menu()
 
     def show_menu(self):
-        exit_program = False
-        while not exit_program:
-            print('\n-------------')
-            print('Main menu')
-            print('-------------')
-            print('1) Generate two lists with zero correlation')
-            print('2) Generate probability of zero correlation')
-            print('3) Close program')
+        print('\n-------------')
+        print('Main menu')
+        print('-------------')
+        print('1) Generate two lists with zero correlation')
+        print('2) Generate probability of zero correlation')
+        print('3) Close program')
 
-            try:
-                option = int(input('\nOption (1-3): '))
-            except ValueError:
-                print('Please enter a valid number.')
-                continue
-
+        try:
+            option = int(input('\nOption (1 or 2): '))
             if option == 1:
                 x, y, tries = ListGen.gen_zero_r()
-                print('List x: {}'.format(x))
-                print('List y: {}'.format(y))
-                print('Number of tries: {}'.format(tries))
+                self.print_results(x, y, tries)
             elif option == 2:
-                print('In progress!')
-            elif option == 3:
-                exit_program = True
+                self.start_again = False
+            else:
+                raise ValueError
+        except ValueError:
+            print('You entered an invalid option.')
+
+    def print_results(self, x, y, tries):
+        print('\nList x: {}'.format(x))
+        print('List y: {}'.format(y))
+        print('Number of tries: {}'.format(tries))
 
 
 if __name__ == "__main__":

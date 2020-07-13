81cf55d84e1104081c7e16382e8cc863697260d0
Mohammed Yaseen <mohammedysn15@gmail.com>
2020-07-13 17:02:17 +0100

    Add comments

--- scripts/list_gen.py
+++ scripts/list_gen.py
@@ -7,19 +7,24 @@ from scipy.stats.stats import pearsonr
 class ListGen:
     @classmethod
     def gen_list(cls, list_length):
+        # Returns a random list of integers from 1 to 5
         return [random.randint(1, 5) for i in range(list_length)]
 
     @classmethod
     def calc_r(cls, list_x, list_y):
-        r = pearsonr(list_x, list_y)[0]
-        return r
+        # Returns pearson r of lists x and y
+        return pearsonr(list_x, list_y)[0]
 
     @classmethod
     def gen_zero_r(cls):
+        # Stores tries
         tries = 0
         while True:
+            # Creates two lists of 12 integers from 1 to 5
             list_x = cls.gen_list(12)
             list_y = cls.gen_list(12)
+            # Increments number of tries
             tries += 1
             if cls.calc_r(list_x, list_y) == 0:
+                # Returns lists and tries if the two lists have zero r
                 return list_x, list_y, tries
--- scripts/main.py
+++ scripts/main.py
@@ -2,11 +2,13 @@ from list_gen import ListGen
 
 
 class Menu():
+    # Creates menu system
     def __init__(self):
         self.start_again = True
         while self.start_again:
             self.show_menu()
 
+    # Shows menu
     def show_menu(self):
         print('\n-------------')
         print('Main menu')
@@ -18,17 +20,22 @@ class Menu():
             option = int(input('\nOption (1 or 2): '))
             if option == 1:
                 x, y, tries = ListGen.gen_zero_r()
+                # Prints two lists and number of tries
                 self.print_results(x, y, tries)
             elif option == 2:
+                # Exits main menu
                 self.start_again = False
             else:
                 raise ValueError
         except ValueError:
+            # Prints error message
             print('You entered an invalid option.')
 
     def print_results(self, x, y, tries):
+        # Prints list x and y
         print('\nList x: {}'.format(', '.join(str(num) for num in x)))
         print('List y: {}'.format(', '.join(str(num) for num in y)))
+        # Prints number of tries
         print('Number of tries: {}'.format(tries))
 
 
